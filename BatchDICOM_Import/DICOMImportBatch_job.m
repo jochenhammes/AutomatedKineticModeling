59c8f661a45c624593a993c7d52ec7a1edd8b315
Jochen Hammes <hammesj@me.com>
2020-06-29 17:56:58 +0200

    V1.0 DicomImport to working directory

--- BatchDICOM_Import/DICOMImportBatch_job.m
+++ BatchDICOM_Import/DICOMImportBatch_job.m
@@ -1,11 +1,13 @@
 %-----------------------------------------------------------------------
-% Job configuration created by cfg_util (rev $Rev: 4252 $)
+% Job saved on 29-Jun-2020 17:37:23 by cfg_util (rev $Rev: 6460 $)
+% spm SPM - SPM12 (6470)
+% cfg_basicio BasicIO - Unknown
 %-----------------------------------------------------------------------
 %%
-matlabbatch{1}.spm.util.dicom.data = {LIST_OF_INPUT_FILES
-                                      };
+matlabbatch{1}.spm.util.import.dicom.data = {LIST_OF_INPUT_FILES};
 %%
-matlabbatch{1}.spm.util.dicom.root = 'flat';
-matlabbatch{1}.spm.util.dicom.outdir = {'PATH_TO_OUTPUT_FOLDER'};
-matlabbatch{1}.spm.util.dicom.convopts.format = 'nii';
-matlabbatch{1}.spm.util.dicom.convopts.icedims = 0;
+matlabbatch{1}.spm.util.import.dicom.root = 'flat';
+matlabbatch{1}.spm.util.import.dicom.outdir = {'PATH_TO_OUTPUT_FOLDER'};
+matlabbatch{1}.spm.util.import.dicom.protfilter = '.*';
+matlabbatch{1}.spm.util.import.dicom.convopts.format = 'nii';
+matlabbatch{1}.spm.util.import.dicom.convopts.icedims = 0;
---
+++ BatchDICOM_Import/fcnDICOMImportBatch.m
@@ -0,0 +1,65 @@
+function runWithSuccess = fcnDICOMImportBatch(pathInputFolder, FilenameProperties, pathOutputFolder)
+
+%   TODO
+%   []  Change WorkingDirectory to a dynamic path
+%   []	Filenameprooerties anpassen
+
+runWithSuccess = false;
+
+%pathInputFolder = '/Volumes/MMNI_RAID/RAID_MMNI/AphasicPatients/DICOM/TV/';
+%pathOutputFolder = pathInputFolder;
+pathOutputFolder = '/DATA/hammesj/Scripts/AutomatedKineticModeling/BatchDICOM_Import/workdir';
+
+
+%Job file for DICOM Import
+
+%Select Files to be normalized, run recursively through all subfolders with **
+clear subj;
+FilenameProperties = '**';
+subj = dir([pathInputFolder FilenameProperties]);
+
+%remove linux folder-dots .. .
+subj(strcmp({subj.name}, '..')) = [];
+subj(strcmp({subj.name}, '.')) = [];
+
+%prepare inputFileList
+inputFileList = '';
+
+for i=1:length(subj)
+    inputFileList = [inputFileList '''' subj(i).folder filesep subj(i).name  '''' char(10)];
+end
+
+fprintf(inputFileList);
+
+
+fin = fopen('DICOMImportBatch_job.m', 'r');
+fout = fopen('DICOMImportBatch_jobINTERMEDIATE.m', 'w');
+
+findstr1 = 'LIST_OF_INPUT_FILES';
+replacestr1 = inputFileList;
+findstr2 = 'PATH_TO_OUTPUT_FOLDER';
+replacestr2 = pathOutputFolder;
+
+while ~feof(fin)
+    s = fgetl(fin);
+    s = strrep(s, findstr1, replacestr1);
+    s = strrep(s, findstr2, replacestr2);
+    
+    fprintf(fout,'%s\n',s)
+end
+
+fclose(fin)
+fclose(fout)
+
+ jobfile = {'DICOMImportBatch_jobINTERMEDIATE.m'};
+ spm('defaults', 'PET');
+ spm_jobman('serial', jobfile);
+
+ 
+ %if ~isempty(subj) > 0
+    runWithSuccess = true;
+ %end
+
+disp('done');
+
+end
