7078608e6cdc90447c82198c9d5c99e70ef87742
Jochen Hammes <hammesj@me.com>
2020-06-29 15:14:00 +0200

    add BatchDicomImport

---
+++ BatchDICOM_Import/DICOMImportBatch_job.m
@@ -0,0 +1,11 @@
+%-----------------------------------------------------------------------
+% Job configuration created by cfg_util (rev $Rev: 4252 $)
+%-----------------------------------------------------------------------
+%%
+matlabbatch{1}.spm.util.dicom.data = {LIST_OF_INPUT_FILES
+                                      };
+%%
+matlabbatch{1}.spm.util.dicom.root = 'flat';
+matlabbatch{1}.spm.util.dicom.outdir = {'PATH_TO_OUTPUT_FOLDER'};
+matlabbatch{1}.spm.util.dicom.convopts.format = 'nii';
+matlabbatch{1}.spm.util.dicom.convopts.icedims = 0;
---
+++ BatchDICOM_Import/DICOMImportBatch_jobINTERMEDIATE.m
@@ -0,0 +1,20 @@
+%-----------------------------------------------------------------------
+% Job configuration created by cfg_util (rev $Rev: 4252 $)
+%-----------------------------------------------------------------------
+%%
+matlabbatch{1}.spm.util.dicom.data = {'/DATA/hammesj/FTLD_AV1451_CGN/19_FTLDCGN/.'
+'/DATA/hammesj/FTLD_AV1451_CGN/19_FTLDCGN/..'
+'/DATA/hammesj/FTLD_AV1451_CGN/19_FTLDCGN/ACCT_19_FTLDCGN.nii'
+'/DATA/hammesj/FTLD_AV1451_CGN/19_FTLDCGN/AV1451_ACCT'
+'/DATA/hammesj/FTLD_AV1451_CGN/19_FTLDCGN/AV1451_PET'
+'/DATA/hammesj/FTLD_AV1451_CGN/19_FTLDCGN/FDG'
+'/DATA/hammesj/FTLD_AV1451_CGN/19_FTLDCGN/FDG_19_FTLDCGN.nii'
+'/DATA/hammesj/FTLD_AV1451_CGN/19_FTLDCGN/TAU_19_FTLDCGN.nii'
+'/DATA/hammesj/FTLD_AV1451_CGN/19_FTLDCGN/rTAU_19_FTLDCGN.nii'
+
+                                      };
+%%
+matlabbatch{1}.spm.util.dicom.root = 'flat';
+matlabbatch{1}.spm.util.dicom.outdir = {'/DATA/hammesj/FTLD_AV1451_CGN/19_FTLDCGN/'};
+matlabbatch{1}.spm.util.dicom.convopts.format = 'nii';
+matlabbatch{1}.spm.util.dicom.convopts.icedims = 0;
